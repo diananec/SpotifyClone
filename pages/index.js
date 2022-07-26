0459c93d21d0ce5c4059e3912955fca458395bb5
Diana Nechita <diananechita1999@gmail.com>
2022-07-26 17:05:42 +0300

    Finished

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
-
+import { getSession } from "next-auth/react";
 import Sidebar from "../components/Sidebar";
 import Center from "../components/Center";
+import Player from "../components/Player";
 
 export default function Home() {
   return (
@@ -16,7 +17,14 @@ export default function Home() {
         <Center />
         {/*Center*/}
       </main>
-      <div>{/*Player*/}</div>
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return { props: { session } };
 }
